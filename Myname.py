ed970292c2b6d09689ddd2166b41d7cb39064925
onkarchoudhary <141794049+onkarchoudhary@users.noreply.github.com>
2023-09-21 11:45:28 +1200

    screen, background color, pen  and speed set

---
+++ Myname.py
@@ -0,0 +1,9 @@
+import turtle
+
+# Create a turtle screen
+window = turtle.Screen()
+window.bgcolor("white")
+
+# Create a turtle
+pen = turtle.Turtle()
+pen.speed(1)  # Set the drawing speed (1 is slowest)
