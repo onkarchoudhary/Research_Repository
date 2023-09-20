95a5c8e2448e9295311b10bfbae620ab781372ab
onkarchoudhary <141794049+onkarchoudhary@users.noreply.github.com>
2023-09-21 11:46:29 +1200

    Making lines thick

--- Myname.py
+++ Myname.py
@@ -7,3 +7,13 @@ window.bgcolor("white")
 # Create a turtle
 pen = turtle.Turtle()
 pen.speed(1)  # Set the drawing speed (1 is slowest)
+
+# Function to draw a thick line
+
+def draw_thick_line(x1, y1, x2, y2, width, color):
+    pen.penup()
+    pen.goto(x1, y1)
+    pen.pendown()
+    pen.pensize(width)
+    pen.pencolor(color)
+    pen.goto(x2, y2)
